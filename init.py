ae7ea6a17ea5a4834bcf758501ccc22ff4d1ac54
xram255 <roshan.cs790@gmail.com>
2020-08-15 21:56:07 +0530

    temperature reading corrected with numpy

--- init.py
+++ init.py
@@ -1,6 +1,7 @@
 
 from time import sleep as dly
 from smbus2 import SMBus
+import numpy as np
 
 I2C_ADDR = 104
 PWR_MNG_REG = 107
@@ -20,17 +21,25 @@ def comb_bytes(inb1, inb2):
     print(temp)
     return temp
 
-def temperature(inval):
-    temp = (inval / 340) + 36.53
-    return temp
+def temperature(inv1, inv2):
+    #print(bin(inv1), "|", inv2)
+    #print(type(np.uint16(inv1)), "|", np.uint16(inv2))
+    #inval = np.uint16(inv1) << 8 | np.uint16(inv2)
+    inval = inv1 << 8 | inv2
+    temp = (np.int16(inval) / 340.00) + 36.53
+    #temp = (inval / 340.00) + 36.53
+    print(temp)
+    #return temp
 
-for i in range(5):
+for i in range(25):
 
     with SMBus(1) as bus:
         block = bus.read_i2c_block_data(I2C_ADDR, STARTING_REG, 2)
-        print(block)
+        #print(block)
 
     print(i)
+    #print(np.int16(block[0] << 8 | block[1]))
+    temperature(block[0], block[1])
     #print(type(block[6]))
     #comb_bytes(block[0], block[1])
     #print(comb_bytes(block[6], block[7]))
