ade2618061bfa06ed02a88d5cb270e7c2343d7f1
xram255 <roshan.cs790@gmail.com>
2020-08-15 20:22:17 +0530

    data received

--- init.py
+++ init.py
@@ -4,16 +4,38 @@ from smbus2 import SMBus
 
 I2C_ADDR = 104
 PWR_MNG_REG = 107
-STARTING_REG = 59
+STARTING_REG = 65
 
 with SMBus(1) as bus:
-    data = [PWR_MNG_REG, 0]
-    bus.write_byte_data(I2C_ADDR, 0, data)
+    bus.write_byte_data(I2C_ADDR, PWR_MNG_REG, 0)
+    dly(1)
 
-with SMBus(1) as bus:
-    data = STARTING_REG
-    bus.write_byte_data(I2C_ADDR, 0, data)
+def comb_bytes(inb1, inb2):
+    inb1 = inb1 & 127
+    #print(bin(inb1))
+    #print(bin(inb2))
+    outb = inb1 << 8 | inb2
+    print(outb)
+    temp = (outb / 340.00) + 36.53
+    print(temp)
+    return temp
 
-with SMBus(1) as bus:
-    block = bus.read_i2c_block_data(I2C_ADDR, 0, 16)
-    print(block)
+def temperature(inval):
+    temp = (inval / 340) + 36.53
+    return temp
+
+for i in range(5):
+
+    with SMBus(1) as bus:
+        block = bus.read_i2c_block_data(I2C_ADDR, STARTING_REG, 2)
+        print(block)
+
+    print(i)
+    #print(type(block[6]))
+    #comb_bytes(block[0], block[1])
+    #print(comb_bytes(block[6], block[7]))
+    #print(block[7] << 8 | block[8] )
+
+    dly(1)
+
+print("end")
